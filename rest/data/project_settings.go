1ec61d8b91ca1f322bb64a6a633f04b5bbe6c2f1
maddaak <akhilesh.maddali@mongodb.com>
2026-04-03 19:56:15 -0400

    PR review fix

--- rest/data/project_settings.go
+++ rest/data/project_settings.go
@@ -79,7 +79,7 @@ func CopyProject(ctx context.Context, env evergreen.Environment, opts restModel.
 		catcher.Wrapf(err, "copying aliases from project '%s'", oldIdentifier)
 	}
 	if err := event.CopyProjectSubscriptions(ctx, oldId, projectToCopy.Id); err != nil {
-		catcher.Add(errors.Errorf("copying subscriptions from project '%s': failed", oldIdentifier))
+		catcher.Errorf("copying subscriptions from project '%s': failed", oldIdentifier)
 	}
 	// Set the same admin roles from the old project on the newly copied project.
 	if err := model.UpdateAdminRoles(ctx, projectToCopy, projectToCopy.Admins, nil); err != nil {
